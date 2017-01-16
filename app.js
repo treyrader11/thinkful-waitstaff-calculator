43ef87a8f83d721b445e63b5415648997b2fedae
Trey Rader <treyrdr09@gmail.com>
2017-01-16 09:37:06 -0600

    injected the ngAnimate module

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-var app = angular.module('mealApp', ['ngMessages', 'ngRoute']);
+var app = angular.module('mealApp', ['ngMessages', 'ngRoute', 'ngAnimate']);
+
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'index.html'
+	})
+	.when('/meal-details', {
+		templateUrl: 'views/meal-details.html',
+		controller: 'MealCtrl'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+}]);
