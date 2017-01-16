39462d36703364fa6a60decbb74afcb568fe51cd
Trey Rader <treyrdr09@gmail.com>
2017-01-16 09:38:49 -0600

    took out  and applied the 'Controller As' syntax

--- controllers/mealCtrl.js
+++ controllers/mealCtrl.js
@@ -1,60 +1,48 @@
-app.controller('MealCtrl', ['$scope', function($scope) {
+app.controller('MealCtrl', function() {
 
-	/*$scope.calcTip = function(total) {
-        var total = total * 1.2;
-        console.log("total for meal with tip is:", total)
-    }
-    
-    $scope.calcTax = function(rate) {
-        var withTax = rate * 1.09;
-        console.log("meal with tax:", withTax)
-        $scope.calcTip(withTax);
-    }
-    
-    $scope.addMeal = function(meal) {
-        $scope.calcTax(meal);    
-    }*/
+    var vm = this;
 
-    initializeForm(); //sets variables for our data
+	initializeForm(); //sets the properties for data object
 
 
-    $scope.addMeal = function() {
+    vm.addMeal = function() {
 
-    	console.log("meal_form:", $scope.meal_form);
+    	console.log("meal_form:", vm.meal_form);
     
         
-	    var meal = $scope.data;
+	    var meal = vm.data;
 	    meal.tax = meal.taxRate * meal.price / 100; //find percent of tax
-	    console.log("the tax is ", meal.tax )
 	    meal.subtotal = meal.price + meal.tax;
-	    console.log("meal with tax is ", meal.subtotal);
 	    meal.tip = meal.subtotal * meal.tipPercentage / 100;
-	    console.log("tip is ", meal.tip);
 	    meal.totalPrice = meal.subtotal + meal.tip;
 	    meal.tipTotal = meal.tip + meal.tipTotal; //adds each tip to each submitted tip
-	    console.log("total number of tips", meal.tipTotal);
 	    meal.mealCount++; //each time the meal is submitted, 1 is added
 	    meal.averageTip = meal.tipTotal / meal.mealCount;
-	    console.log("the data:", meal)
+	    console.log("the data:", meal);
+
+        meal.taxRate = 0;
+        meal.price = 0;
+        meal.tipPercentage = 0;
+        vm.meal_form.$setPristine();
     	
     }
 
-    $scope.reset = function() {
+    vm.reset = function() {
         initializeForm();
-        $scope.mealForm.$setPristine();
+        vm.meal_form.$setPristine();
     }
 
-    $scope.cancel = function() {
-        var meal = $scope.data;
+    vm.cancel = function() {
+        var meal = vm.data;
         meal.price = 0;
         meal.taxRate = 0;
         meal.tipPercentage = 0;
-        $scope.mealForm.taxRate = 0;
-        $scope.mealForm.$setPristine();
+        vm.meal_form.taxRate = 0;
+        vm.meal_form.setPristine;
     }
  
     function initializeForm() {
-        $scope.data = {
+        vm.data = {
             price: 0,
             taxRate: 0,
             tipPercentage: 0,
@@ -67,4 +55,4 @@ app.controller('MealCtrl', ['$scope', function($scope) {
             averageTip: 0
         }
     }
-}])
+});
